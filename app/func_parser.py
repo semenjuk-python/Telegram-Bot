76d114a9cbf35c635836451f5045b2cb9e98ca8b
Максим Семенюк <semenjukmaksim@gmail.com>
2017-04-11 19:13:33 +0300

    fix problems when site was not responding

--- app/func_parser.py
+++ app/func_parser.py
@@ -20,10 +20,10 @@ def message_disp(bot, update):
     else:
         unknown(bot=bot,update=update)
 
-#the first rates' source
 def obmenka(bot,update):
     (currencies,date)=obmenka_parse()
-    text='''Курс валют  \n%s :\n\t
+    if currencies!=None:
+        text='''Курс валют  \n%s :\n\t
             *$ Доллар $*\n    _Покупка / Продажа_
           *%s* \t/\t *%s*\n
             *€ Евро €*\n    _Покупка / Продажа_
@@ -47,19 +47,24 @@ def obmenka(bot,update):
               str(currencies[4][2]),
               str(currencies[4][3])
               )
-    bot.sendMessage(chat_id=update.message.chat_id,
-                    text=text,
-                    parse_mode=ParseMode.MARKDOWN,
-                    reply_markup=keyboard_show,
-                    )
+        bot.sendMessage(chat_id=update.message.chat_id,
+                        text=text,
+                        parse_mode=ParseMode.MARKDOWN,
+                        reply_markup=keyboard_show,
+                        )
+    else:
+        bot.sendMessage(chat_id=update.message.chat_id,
+                        text='Sorry the site didn\'t respond.Try another one.',
+                        parse_mode=ParseMode.MARKDOWN,
+                        reply_markup=keyboard_show,
+                        )
 
-#the second rates' source
+
 def minfin(bot,update):
-    (html, success)=minfin_get_html()
+    (htmlcash, success, htmlbank)=minfin_get_html()
     if success==True:
-        (curr, month, date)=minfin_parse(html)
-        text='''
-                Курс валют  %s %s %s:\n\n\t    *НАЛИЧНЫЙ РЫНОК*
+        (curr)=minfin_parse(htmlcash,htmlbank)
+        text='''Курс валют  :\n\n\t    *НАЛИЧНЫЙ РЫНОК*
             *$ Доллар $*\n _Покупка / Продажа_
         *%s* \t/\t *%s*\n
             *€ Евро €*\n _Покупка / Продажа_
@@ -70,11 +75,10 @@ def minfin(bot,update):
             *$ Доллар $*\n _Покупка / Продажа_
         *%s* \t/\t *%s*\n
             *€ Евро €*\n _Покупка / Продажа_
-        *%s* \t/\t *%s \n\n\t\t      Хорошего дня !*
-                ''' %(str(date[0]),
-                      month.encode('utf-8'),
-                      str(date[1]),
-                      str(curr[0][0]),
+        *%s* \t/\t *%s*\n
+            *₽ Рубль ₽*\n _Покупка / Продажа_
+        *%s* \t/\t *%s*  \n\n\t\t      *Хорошего дня !*
+                '''%(str(curr[0][0]),
                       str(curr[0][1]),
                       str(curr[1][0]),
                       str(curr[1][1]),
@@ -83,7 +87,9 @@ def minfin(bot,update):
                       str(curr[3][0]),
                       str(curr[3][1]),
                       str(curr[4][0]),
-                      str(curr[4][1])
+                      str(curr[4][1]),
+                      str(curr[5][0]),
+                      str(curr[5][1])
                       )
         bot.sendMessage(chat_id=update.message.chat_id,
                         text=text,
@@ -92,10 +98,9 @@ def minfin(bot,update):
                         )
     else:
         bot.sendMessage(chat_id=update.message.chat_id,
-                        text=html,
+                        text='Sorry the site didn\'t respond.Try another one.',
                         reply_markup=keyboard_show,
                         )
-
 def unknown(bot,update):
     bot.sendMessage(chat_id=update.message.chat_id,
         reply_to_message_id=update.message.message_id,
@@ -110,7 +115,6 @@ unknown_handler=MessageHandler(Filters.command | Filters.text, unknown)
 handlers=[start_handler,
          main_handler,
          unknown_handler]
-
 
 #ERROR handlers
 from telegram.error import (TelegramError, Unauthorized, BadRequest, TimedOut,
--- app/parser.py
+++ app/parser.py
@@ -7,7 +7,9 @@ def minfin_get_html():
                 Gecko/20100101 Firefox/45.0'''
              }
     try:
-        html=urlopen('http://minfin.com.ua/currency/').read()
+        htmlcash=urlopen('http://minfin.com.ua/currency/').read()
+        htmlbank=urlopen('http://minfin.com.ua/currency/mb/').read()
+
     except ValueError:
         html='We regret but something went wrong...Try later.'
         success=False
@@ -16,11 +18,11 @@ def minfin_get_html():
         success=False
     else:
         success=True
-        html=html[180000:]
-        start=html.find('<td class="mfcur-table-cur">')
-        end=html.find('<h4 class="mfm-h4 mfcur-news-h">')
-        html=html[start:end]
-    return html, success
+        htmlcash=htmlcash[180000:]
+        start=htmlcash.find('<td class="mfcur-table-cur">')
+        end=htmlcash.find('<h4 class="mfm-h4 mfcur-news-h">')
+        htmlcash=htmlcash[start:end]
+    return htmlcash, success, htmlbank
 
 def minfin_init_cash(values):
     numbers=values
@@ -30,16 +32,29 @@ def minfin_init_cash(values):
     ruble=(numbers[16],numbers[19])
     return dollar, euro, ruble
 
-def minfin_init_bank(values):
-    numbers=values
+def minfin_init_bank(htmlbank):
+    start=htmlbank.find('<table class="mb-table-currency">')
+    end=htmlbank.find('<div class="mb-interbank-graph--body">')
+    htmlbank=htmlbank[start:end]
+    soup=BeautifulSoup(htmlbank,'html.parser')
+    pure=soup.get_text()
+    numbers=str()
+    for i in range(len(pure)):
+        if pure[i].isdigit() and pure[i+1].isdigit():
+            numbers=numbers+pure[i]
+        elif pure[i].isdigit() and (pure[i+1]==',' or pure[i+1]=='.'):
+            numbers+=pure[i]+pure[i+1]
+            i+=1
+        elif pure[i].isdigit() and not pure[i+1].isdigit() and pure[i+1] not in (',','.'):
+            numbers=numbers+pure[i]+' '
     numbers=numbers.split(' ')
-    dollar=(numbers[50],numbers[51])
-    euro=(numbers[54],numbers[55])
-    date=(numbers[52],numbers[53])
-    return dollar, euro, date
+    dollar=(numbers[0],numbers[6])
+    euro=(numbers[2],numbers[8])
+    ruble=(numbers[4],numbers[10])
+    return dollar, euro, ruble
 
-def minfin_parse(html):
-    soup=BeautifulSoup(html,'html.parser')
+def minfin_parse(htmlcash,htmlbank):
+    soup=BeautifulSoup(htmlcash,'html.parser')
     pure=soup.get_text()
     numbers=str()
     words=str()
@@ -57,26 +72,32 @@ def minfin_parse(html):
         elif pure[i].isdigit() and not pure[i+1].isdigit() and pure[i+1] not in (',','.'):
             numbers=numbers+pure[i]+' '
     l=len(words)-2
-    month=str()
-    while words[l]!=' ':
-        month=words[l]+month
-        l-=1
-    (dollar_bank, euro_bank, date)=minfin_init_bank(numbers)
+
+    (dollar_bank, euro_bank, ruble_bank)=minfin_init_bank(htmlbank)
     (dollar_cash, euro_cash, ruble_cash)=minfin_init_cash(numbers)
     currencies=(
                 dollar_cash,
                 euro_cash,
                 ruble_cash,
                 dollar_bank,
-                euro_bank
+                euro_bank,
+                ruble_bank
                 )
-    return currencies, month, date
+    return currencies
 
 from requests import get
 from json import loads
+from requests.exceptions import ConnectionError
 
 def obmenka_parse():
-    r=get('https://obmenka.kharkov.ua/api/rates/020016')
+    try:
+        r=get('https://obmenka.kharkov.ua/api/rates/020016')
+        data=loads(r.text)
+    except ValueError:
+        full_set=None
+        date=None
+        return full_set, date
+
     data=loads(r.text)
     full_set=list()
     date=data[0].get('latestRates').get('createdAt')
